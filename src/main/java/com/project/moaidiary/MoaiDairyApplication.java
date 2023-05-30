d6cc8430b811ff7f95864170ec202bd74fb76ec9
serin <serin4723@naver.com>
2023-05-31 01:07:38 +0900

    init settings

---
+++ src/main/java/com/project/moaidiary/MoaiDairyApplication.java
@@ -0,0 +1,13 @@
+package com.project.moaidiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaiDairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaiDairyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/moaidiary/MoaiDairyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moaidiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaiDairyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
