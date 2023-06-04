0fbd0bc1ce366b0a939084282057aff11453c654
Serin Kim <68180798+SRin23@users.noreply.github.com>
2023-06-05 02:21:32 +0900

    [#1] 회원 가입 API 개발 (#13)

--- src/main/java/com/project/moaidiary/service/user/UserService.java
+++ src/main/java/com/project/moaidiary/service/user/UserService.java
@@ -1,6 +1,8 @@
 package com.project.moaidiary.service.user;
 
+import com.project.moaidiary.entity.user.User;
 import com.project.moaidiary.entity.user.UserRepository;
+import com.project.moaidiary.service.user.dto.CreateUserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public void createUser(CreateUserDto createUserDto) {
+        userRepository.save(User.from(createUserDto));
+    }
 }
