db830e46cff6fb60090a80388060cf64abba61a8
serin <serin4723@naver.com>
2023-06-08 02:52:42 +0900

    배포 테스트

--- src/main/java/com/project/moaidiary/entity/user/User.java
+++ src/main/java/com/project/moaidiary/entity/user/User.java
@@ -1,6 +1,5 @@
 package com.project.moaidiary.entity.user;
 
-import com.project.moaidiary.moai_enum.ImageProfile;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -15,22 +14,4 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long userId;
-
-    @Column(name = "user_name", nullable = false, length = 20)
-    private String userName;
-
-    @Column(name = "user_display_name", nullable = false, length = 50, unique = true)
-    private String userDisplayName;
-
-    @Column(name = "user_email", nullable = false, length = 100, unique = true)
-    private String userEmail;
-
-    @Column(name = "user_phone", length = 15)
-    private String userPhone;
-
-    @Column(name = "firebase_unique_key", nullable = false, length = 30)
-    private String firebaseUniqueKey;
-
-    @Column(name = "image_profile_name")
-    private ImageProfile imageProfileName;
 }
