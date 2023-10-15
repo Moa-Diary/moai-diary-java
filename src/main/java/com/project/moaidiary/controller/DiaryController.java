af36d5b625c1f3f618c54c20774913ad6edfc94b
serin <serin4723@naver.com>
2023-10-16 02:12:52 +0900

    [#29] 일기 공개 범위 수정 API 개발

--- src/main/java/com/project/moaidiary/controller/DiaryController.java
+++ src/main/java/com/project/moaidiary/controller/DiaryController.java
@@ -2,20 +2,24 @@ package com.project.moaidiary.controller;
 
 import com.project.moaidiary.service.diary.dto.DiaryCombineService;
 import com.project.moaidiary.service.diary.dto.DiaryCountDto;
+import com.project.moaidiary.service.diary.dto.DiaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/diary")
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryCombineService diaryCombineService;
+    private final DiaryService diaryService;
 
     @GetMapping("/user/{userId}/count")
     public DiaryCountDto diaryCount(@PathVariable Long userId) {
         return diaryCombineService.countDiaryByUserEmail(userId);
+    }
+
+    @PutMapping("/{diaryId}/public/{isPublic}")
+    public void changeIsPublic(@PathVariable Long diaryId, @PathVariable Boolean isPublic) {
+        diaryService.modifyDiaryIsPublic(diaryId, isPublic);
     }
 }
--- src/main/java/com/project/moaidiary/exception/CustomErrorCode.java
+++ src/main/java/com/project/moaidiary/exception/CustomErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum CustomErrorCode {
-    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "회원 정보가 존재하지 않습니다.");
+    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "회원 정보가 존재하지 않습니다."),
+    NOT_EXIST_DIARY(HttpStatus.BAD_REQUEST, "일기 정보가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String statusMessage;
